df5fd176d1b6a0678769a0701d557e6859483305
+ <+>
2023-06-07 15:28:40 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a,b){
+return a + b
+}
+function subtract(a,b){
+    return a - b
+}
+function multiply(a,b){
+    return a * b
+}
+function divide(a,b){
+    return a/b
+}
+function increment (n){
+     n++
+     return n
+}
+function decrement (n){
+    n--
+    return n
+}
+function makeInt(n){
+    return parseInt(n, 10)    
+}
+makeInt (a)
+function preserveDecimal (n){
+    return parseFloat(n)
+}
